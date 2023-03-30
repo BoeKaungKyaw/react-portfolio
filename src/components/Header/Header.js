2b5151284a4ef155cb54e65b484c4662cdbcf0f3
Boe_Kaung_Kyaw <boekaung.kyaw@kbzbank.com>
2023-03-30 16:04:59 +0630

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,50 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+// import { DiCssdeck } from 'react-icons/di';
+import { FcBiotech } from "react-icons/fc";
+// FcBiotech
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons,Span} from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href='/'>
+        <a style={{display: "flex", alignItems: "center", color: "white",marginBottom:"20px" }}>
+        <FcBiotech size="3rem"/><Span>BKK</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Project</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#teach">
+          <NavLink>Technology</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href=''>
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href=''>
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href=''>
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
